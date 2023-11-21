39fbe28b4f146af390305491f26a1493b09c61dc
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-11-21 07:16:38 +0100

    Update GitHub credentials

--- pwa/config/github.ts
+++ pwa/config/github.ts
@@ -1,2 +1,2 @@
-export const GITHUB_CLIENT_ID: string = process.env.GITHUB_CLIENT_ID || 'api-platform-pwa';
-export const GITHUB_SECRET: string = process.env.GITHUB_SECRET || 'https://localhost/oidc/realms/demo';
+export const GITHUB_CLIENT_ID: string = process.env.GITHUB_CLIENT_ID;
+export const GITHUB_SECRET: string = process.env.GITHUB_SECRET;
