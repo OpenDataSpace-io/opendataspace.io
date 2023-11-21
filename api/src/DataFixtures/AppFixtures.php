389fc28630e03713e08c0c6f81e2d0e6c37585ae
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-11-21 08:56:42 +0100

    Add ThingStory to AppFixtures

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\DataFixtures;
 
 use App\DataFixtures\Story\DefaultStory;
+use App\DataFixtures\Story\ThingStory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -13,5 +14,6 @@ final class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         DefaultStory::load();
+        ThingStory::load();
     }
 }
