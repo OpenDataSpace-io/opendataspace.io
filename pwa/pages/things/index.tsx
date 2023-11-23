b380589865a0b240179f67b899039922c634a2c0
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-11-23 19:42:43 +0100

    Refactor API integration in ThingsPage component

--- pwa/pages/things/index.tsx
+++ pwa/pages/things/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 
 import { HydraAdmin, fetchHydra, hydraDataProvider, ResourceGuesser } from "@api-platform/admin";
 import { parseHydraDocumentation } from "@api-platform/api-doc-parser";
+import { Admin, Resource } from 'react-admin';
 
 //const entrypoint = process.env.NEXT_PUBLIC_ENTRYPOINT;
 const entrypoint: string = typeof window === "undefined" ? process.env.NEXT_PUBLIC_ENTRYPOINT : window.origin;
@@ -22,11 +23,9 @@ const ThingsPage = () => {
       </Head>
       <h1>ThingsPage</h1>
 
-      <HydraAdmin
-        dataProvider={dataProvider}
-        entrypoint={entrypoint}>
-          <ResourceGuesser name="things" />
-        </HydraAdmin>
+      <Admin dataProvider={dataProvider}>
+        <Resource name="things" />
+      </Admin>
     </div>
   );
 };
