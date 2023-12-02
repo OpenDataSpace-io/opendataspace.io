805418141008d2dc96eafd53a021f090f4d49527
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-12-02 10:04:08 +0100

    Refactor ThingNormalizer to handle different data formats

--- api/src/Serializer/Normalizer/ThingNormalizer.php
+++ api/src/Serializer/Normalizer/ThingNormalizer.php
@@ -20,61 +20,25 @@ class ThingNormalizer implements NormalizerInterface, CacheableSupportsMethodInt
         
         $dateCreated = $data['dateCreated'];
         $dateModified = $data['dateModified'];
-        if (isset($data['@id']))
-        {
-            $id = $data['@id'];
+
+        if(isset($data['properties'][0])){
+            $data = $data['properties'];
         }
 
-                if($format == 'json'){
-                    if(isset($data['properties'][0])){
-                        $data = $data['properties'][0];
-                    }
-                }
-                if($format == 'jsonld'){
-                    if (isset($data['properties']['hydra:member'][0])){
-                        $data = $data['properties']['hydra:member'][0];
-                    }
-                }
-        if (isset($id)){
-            $data['@id'] = $id;
+        if (isset($data['properties']['hydra:member'][0])){
+            $data = $data['properties']['hydra:member'][0];
         }
-
-        // TODO: Add @id to properties --> Datafixtures
-        if(isset($context['operation_name']))
-        {
-            if($context['operation_name'] == "_api_/things/{id}{._format}_get")
-            {
-                $data['@id'] = $context['uri_variables']['id'];
-            }
-        }
+        
+        $data["@context"] = "https://schema.org/";
+        // TODO: Set Type from properties['@type']
+        $data['@type'] = 'Thing';
+        $data['@id'] = $object->getId();
+        
         $data['dateCreated'] = $dateCreated;
         $data['dateModified'] = $dateModified;
-        // Debug
+        // DEBUG
         $data['context'] = $context;
-
-
-
-        
-
-        /*if (isset($context['api_denormalize']) && $context['api_denormalize'] === true) {
-            // GET_COLLECTION Anforderung
-            // Modifizieren Sie die Daten entsprechend
-        } else {
-            // GET
-            $dateCreated = $data['dateCreated'];
-            $dateModified = $data['dateModified'];
-            if($format == 'json'){
-                $data = $data['properties'][0];
-
-            }
-            if($format == 'jsonld'){
-                $data = $data['properties']['hydra:member'][0];
-            }
-            $data['dateCreated'] = $dateCreated;
-            $data['dateModified'] = $dateModified;
-        }*/
-        
-
+        $data['format'] = $format;
         return $data;
     }
 
