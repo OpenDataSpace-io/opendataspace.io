cf37083fec517523fc28145d2514e696b19bba51
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-12-02 07:30:13 +0100

    Refactor ThingNormalizer to handle different formats and add @id to properties

--- api/src/Serializer/Normalizer/ThingNormalizer.php
+++ api/src/Serializer/Normalizer/ThingNormalizer.php
@@ -17,6 +17,44 @@ class ThingNormalizer implements NormalizerInterface, CacheableSupportsMethodInt
     public function normalize($object, string $format = null, array $context = []): array
     {
         $data = $this->normalizer->normalize($object, $format, $context);
+        
+        $dateCreated = $data['dateCreated'];
+        $dateModified = $data['dateModified'];
+        if (isset($data['@id']))
+        {
+            $id = $data['@id'];
+        }
+
+                if($format == 'json'){
+                    if(isset($data['properties'][0])){
+                        $data = $data['properties'][0];
+                    }
+                }
+                if($format == 'jsonld'){
+                    if (isset($data['properties']['hydra:member'][0])){
+                        $data = $data['properties']['hydra:member'][0];
+                    }
+                }
+        if (isset($id)){
+            $data['@id'] = $id;
+        }
+
+        // TODO: Add @id to properties --> Datafixtures
+        if(isset($context['operation_name']))
+        {
+            if($context['operation_name'] == "_api_/things/{id}{._format}_get")
+            {
+                $data['@id'] = $context['uri_variables']['id'];
+            }
+        }
+        $data['dateCreated'] = $dateCreated;
+        $data['dateModified'] = $dateModified;
+        // Debug
+        $data['context'] = $context;
+
+
+
+        
 
         /*if (isset($context['api_denormalize']) && $context['api_denormalize'] === true) {
             // GET_COLLECTION Anforderung
