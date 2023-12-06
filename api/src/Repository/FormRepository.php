d93adffe83e54aa219539bc24d683b246efa01c5
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-12-06 17:03:18 +0100

    Add save() and remove() methods to FormRepository

--- api/src/Repository/FormRepository.php
+++ api/src/Repository/FormRepository.php
@@ -21,6 +21,24 @@ class FormRepository extends ServiceEntityRepository
         parent::__construct($registry, Form::class);
     }
 
+    public function save(Form $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Form $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
 //    /**
 //     * @return Form[] Returns an array of Form objects
 //     */
