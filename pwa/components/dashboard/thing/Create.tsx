378c0fea95a83db25bc0c19b6246af7bb1a685eb
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-12-14 20:32:03 +0100

    Delete unused files and components

--- pwa/components/dashboard/thing/Create.tsx
+++
@@ -1,9 +0,0 @@
-import { CreateGuesser, type CreateGuesserProps } from "@api-platform/admin";
-
-import { Form } from "@/components/dashboard/thing/Form";
-
-export const Create = (props: CreateGuesserProps) => (
-  <CreateGuesser {...props} title="Create thing">
-    <Form/>
-  </CreateGuesser>
-);
--- pwa/components/dashboard/thing/JsonTextInput.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { TextInput } from 'react-admin';
-
-const JsonTextInput = props => (
-    <TextInput {...props} format={v => JSON.stringify(v, null, 2)} parse={v => JSON.parse(v)} />
-);
-
-export default JsonTextInput;
