0c07efc711deb2ad8b3d765d2937f30ec85e1994
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-12-16 07:26:49 +0100

    Add environment variable support for GitHub, Keycloak, and OpenStreetMap configurations

--- pwa/config/openstreetmap.ts
+++ pwa/config/openstreetmap.ts
@@ -1,2 +1,4 @@
+// @ts-ignore
 export const OPENSTREETMAP_CLIENT_ID: string = process.env.OPENSTREETMAP_CLIENT_ID;
+// @ts-ignore
 export const OPENSTREETMAP_CLIENT_SECRET: string = process.env.OPENSTREETMAP_CLIENT_SECRET;
