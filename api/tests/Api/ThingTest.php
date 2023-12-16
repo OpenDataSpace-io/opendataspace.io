cfc62d12d53bd7903afa7f085748cc4a1e5776bd
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-12-16 07:02:54 +0100

    Refactor ThingTest.php to create and order Things

--- api/tests/Api/ThingTest.php
+++ api/tests/Api/ThingTest.php
@@ -30,8 +30,8 @@ final class ThingTest extends ApiTestCase
     public function testAsAnonymousICanGetACollectionOfThings(FactoryCollection $factory, string $url, int $hydraTotalItems): void
     {
         // Cannot use Factory as data provider because ThingFactory has a service dependency
-        //$factory->create();
-        ThingFactory::createMany(35);
+        $factory->create();
+        //ThingFactory::createMany(35);
 
         $response = $this->client->request('GET', $url);
 
@@ -73,15 +73,15 @@ final class ThingTest extends ApiTestCase
 
     public function testAsAdminUserICanGetACollectionOfThingsOrderedByName(): void
     {
-        ThingFactory::createOne(['name' => 'AC Thing Hyperion']);
-        ThingFactory::createOne(['name' => 'AB Thing The Wandering Earth']);
-        ThingFactory::createOne(['name' => 'AA Thing Ball Lightning']);
+        ThingFactory::createOne(['name' => 'Thing AC']);
+        ThingFactory::createOne(['name' => 'Thing AB']);
+        ThingFactory::createOne(['name' => 'Thing AA']);
 
         $response = $this->client->request('GET', '/things?order[name]=asc');
 
         self::assertResponseIsSuccessful();
         self::assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
-        self::assertEquals('AA Thing Ball Lightning', $response->toArray()['hydra:member'][0]['name']);
+        self::assertEquals('Thing AA', $response->toArray()['hydra:member'][0]['name']);
         //self::assertEquals('AC Thing Hyperion', $response->toArray()['hydra:member'][1]['name']);
         //self::assertEquals('AB Thing The Wandering Earth', $response->toArray()['hydra:member'][2]['name']);
     }
