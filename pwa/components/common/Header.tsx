d43eca47dc48eabc23d56726d9b29a5104238832
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-12-22 10:00:18 +0100

    Update store links in header and homepage

--- pwa/components/common/Header.tsx
+++ pwa/components/common/Header.tsx
@@ -16,15 +16,11 @@ export const Header = () => {
     <header className="bg-neutral-100 sticky top-0 z-10">
       <nav className="container mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="block text-4xl font-bold">
-          <Link href="/books" className="text-gray-700 hover:text-gray-900">
-            Books Store
+          <Link href="/things" className="text-gray-700 hover:text-gray-900">
+            Thing Store
           </Link>
         </div>
         <div className="lg:flex lg:flex-1 lg:justify-end lg:gap-x-12">
-          <Link href="/bookmarks" className="font-semibold text-gray-700 hover:text-gray-900 mr-4">
-            <FavoriteBorderIcon className="w-6 h-6 mr-1"/>
-            My Bookmarks
-          </Link>
           {/* @ts-ignore */}
           {!!session && !session.error && (
             <a href="#" className="font-semibold text-gray-900" role="menuitem" onClick={(e) => {
--- pwa/pages/index.tsx
+++ pwa/pages/index.tsx
@@ -67,10 +67,10 @@ const Welcome = () => (
           </p>
           <div className="flex justify-center flex-wrap | lg:justify-start lg:grid lg:gap-5 lg:grid-cols-2">
             <Link
-              href={"/books"}
+              href={"/things"}
               className="bg-white text-cyan-700 px-8 py-3 relative overflow-hidden transition-all font-extrabold text-lg group hover:pl-4 hover:pr-12"
             >
-              Visit the Books Store
+              Visit the Things Store
               <div className="absolute left-full top-0 w-7 h-full bg-cyan-200 transition-all flex p-1 justify-center items-center group-hover:-translate-x-full">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
