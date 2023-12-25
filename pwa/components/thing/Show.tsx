b068c667544d9a327e03da0b87e4fb1ef5b871a9
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-12-25 05:24:58 +0100

    Add item interface to Show component

--- pwa/components/thing/Show.tsx
+++ pwa/components/thing/Show.tsx
@@ -29,6 +29,12 @@ interface Props {
   page: number;
 }
 
+interface item {
+  name: string;
+  description: string;
+  image: string;
+}
+
 export const Show: NextPage<Props> = ({ data, hubURL, page }) => {
 
   const item = useMercure(data, hubURL);
