535245891f5b8513c37411b23adc517bcfb8188b
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-01-02 09:55:02 +0100

    Delete unused files and classes

--- pwa/types/OpenLibrary/Search.ts
+++
@@ -1,8 +0,0 @@
-import { SearchDoc } from "@/types/OpenLibrary/SearchDoc";
-
-export class Search {
-    constructor(
-        public docs: Array<SearchDoc>,
-    ) {
-    }
-}
--- pwa/types/OpenLibrary/Work.ts
+++
@@ -1,9 +0,0 @@
-import { type Description } from "@/types/OpenLibrary/Description";
-
-export class Work {
-  constructor(
-    public description?: string | Description,
-    public covers?: Array<number>,
-  ) {
-  }
-}
