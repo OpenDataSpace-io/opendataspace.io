baa28f4aa47cbf63f81159f2ebf8c9b01fedd7de
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-01-06 10:03:41 +0100

    Update homepage title in test

--- pwa/tests/Homepage.spec.ts
+++ pwa/tests/Homepage.spec.ts
@@ -6,47 +6,7 @@ test.describe("Homepage", () => {
   });
 
   test("Check homepage @read", async ({ page }) => {
-    await expect(page).toHaveTitle("Welcome to API Platform!");
-  });
-
-  test("Go to Les-Tilleuls.coop website @read", async ({ browser, page }) => {
-    const [newPage] = await Promise.all([
-      page.context().waitForEvent("page"),
-      await page.getByRole("link", { name: "Made with by Les-Tilleuls.coop" }).click(),
-    ]);
-    await expect(newPage).toHaveURL(/^https:\/\/les-tilleuls\.coop/);
-  });
-
-  test("Go to Next.js @read", async ({ page }) => {
-    const [newPage] = await Promise.all([
-      page.context().waitForEvent("page"),
-      await page.getByRole("link", { name: "Next.js" }).click(),
-    ]);
-    await expect(newPage).toHaveURL(/^https:\/\/nextjs\.org/);
-  });
-
-  test("Go to API Platform docs @read", async ({ page }) => {
-    const [newPage] = await Promise.all([
-      page.context().waitForEvent("page"),
-      await page.getByRole("link", { name: "Get started" }).click(),
-    ]);
-    await expect(newPage).toHaveURL(/^https:\/\/api-platform\.com\/docs\//);
-  });
-
-  test("Go to Twitter @read", async ({ page }) => {
-    const [newPage] = await Promise.all([
-      page.context().waitForEvent("page"),
-      await page.getByRole("link", { name: "API Platform on Twitter" }).click(),
-    ]);
-    await expect(newPage).toHaveURL("https://twitter.com/ApiPlatform");
-  });
-
-  test("Go to Mastodon @read", async ({ page }) => {
-    const [newPage] = await Promise.all([
-      page.context().waitForEvent("page"),
-      await page.getByRole("link", { name: "API Platform on Mastodon" }).click(),
-    ]);
-    await expect(newPage).toHaveURL("https://fosstodon.org/@ApiPlatform");
+    await expect(page).toHaveTitle("OpenDataSpace - Things");
   });
 
   test("Go to API docs @read", async ({ page }) => {
