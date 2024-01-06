cb35a49658dc655c5b7e9e061c69987f5695731d
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-01-06 13:51:47 +0100

    Add new translations for "new" object in common.json

--- pwa/components/thing/New.tsx
+++ pwa/components/thing/New.tsx
@@ -1,12 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import { ErrorSchema, RJSFSchema, RJSFValidationError, UiSchema, ValidatorType } from '@rjsf/utils';
-import validator from '@rjsf/validator-ajv8';
-import Form from '@rjsf/mui';
+import { type NextPage } from "next";
 import Head from "next/head";
-import Editors from '@/components/form/Editors';
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState, useCallback, ComponentType } from "react";
+import { useMutation } from "react-query";
+import Typography from "@mui/material/Typography";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import { Container, Grid, Button } from '@mui/material';
 import { signIn, useSession } from "next-auth/react";
+
+import { type Thing } from "@/types/Thing";
+import { useMercure } from "@/utils/mercure";
+//import { fetch, type FetchError, type FetchResponse } from "@/utils/dataAccess";
+import { type PagedCollection } from "@/types/collection";
+import { Loading } from "@/components/common/Loading";
+import Editors from '@/components/form/Editors';
+import CustomForm from '@/components/form/CustomForm';
+
+import { ErrorSchema, RJSFSchema, RJSFValidationError, UiSchema, ValidatorType } from '@rjsf/utils';;
+import validator from '@rjsf/validator-ajv8';
+import { FormProps, IChangeEvent, withTheme } from '@rjsf/core';
+import Form from '@rjsf/mui';
+import { useRouter } from 'next/router';
+
 import { useTranslation } from 'next-i18next';
+
+interface Props {
+    data: Thing;
+    hubURL: string | null;
+    page: number;
+}
 
 interface Session {
     accessToken: string;
@@ -19,7 +44,12 @@ const AUTHORIZATION_HEADER = 'Authorization';
 const BEARER_PREFIX = 'Bearer';
 const APPLICATION_JSON = 'application/json';
 
-const NewThingForm = () => {
+// https://rjsf-team.github.io/react-jsonschema-form/docs/
+
+export const New: NextPage<Props> = ({ data, hubURL, page }) => {
+    const { data: session = { accessToken: '', error: '' }, status } = useSession() || {};
+    //const {session, status } = useSession();
+    //const item = useMercure(data, hubURL);
     const [schema, setSchema] = useState({});
     const [uiSchema, setUiSchema] = useState({});
     const [formData, setFormData] = useState({});
@@ -27,13 +57,15 @@ const NewThingForm = () => {
     const [formList, setFormList] = useState([]);
     const [extraErrors, setExtraErrors] = useState<ErrorSchema | undefined>();
     const [shareURL, setShareURL] = useState<string | null>(null);
-    const [isGridVisible, setGridVisible] = useState(false);
-    const { data: session = { accessToken: '', error: '' }, status } = useSession() || {};
+    const [isGridVisible, setGridVisible] = useState(false); // Changed initial value to false
+    const [FormComponent, setFormComponent] = useState<ComponentType<FormProps>>(withTheme({}));
+    const { t } = useTranslation('common');
+    const router = useRouter();
 
     const handleButtonClick = () => {
         setGridVisible(!isGridVisible);
     };
-    
+
     // Fetch the list of available forms from the API
     useEffect(() => {
         const fetchFormList = async () => {
@@ -83,11 +115,11 @@ const NewThingForm = () => {
         }
     }, [selectedForm]);
 
-    const handleFormSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const handleFormSelect = (e : any) => {
         setSelectedForm(e.target.value);
     };
 
-    const handleFormDataChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleFormDataChange = (e : any) => {
         try {
             const newFormData = JSON.parse(e.target.value);
             setFormData(newFormData);
@@ -96,16 +128,28 @@ const NewThingForm = () => {
         }
     };
 
+    const onFormDataChange = useCallback(
+        ({ formData }: IChangeEvent, id?: string) => {
+            if (id) {
+                console.log('Field changed, id: ', id);
+            }
+
+            setFormData(formData);
+            setShareURL(null);
+        },
+        [setFormData, setShareURL]
+    );
+
     const handleSubmit = async (data: any) => {
         // Handle form submission here
         console.log(data);
         console.log(data["formData"]);
 
         try {
-            //if (!session) return;
+            if (!session) return; // Guard against missing session
             //@ts-ignore
             const token = session.accessToken; // Get the authentication token from the session
-            const response = await fetch('/things', {
+            const response = await fetch(`/things`, {
                 method: METHOD,
                 headers: {
                     [CONTENT_TYPE_HEADER]: APPLICATION_JSON,
@@ -117,70 +161,103 @@ const NewThingForm = () => {
             if (response.ok) {
                 // Handle successful response
                 console.log('Thing created successfully');
+                // go to the thing page
+                data = await response.json();
+                router.push(data['@id']);
             } else {
                 // Handle error response
-                console.error('Error creating thing:', response.statusText);
+                throw new Error(`Error creating thing: ${response.statusText}`);
             }
         } catch (error) {
             console.error('Error creating thing:', error);
         }
     };
 
-    return (
-        <>
-            <Head>
-                <title>New Thing</title>
-            </Head>
-            <Container maxWidth="xl">
-            <Button onClick={handleButtonClick}>
-                {isGridVisible ? 'Hide Expert Mode' : 'Show Expert Mode'}
-            </Button>
-                <Grid container spacing={2}>
-                    <Grid item xs={isGridVisible ? 6 : 12} md={isGridVisible ? 8 : 12}>
-                        {selectedForm && (
-                        <>
-                            <div>
-                                <Form
-                                    schema={schema}
-                                    uiSchema={uiSchema}
-                                    formData={formData}
-                                    validator={validator}
-                                    onSubmit={handleSubmit}
-                                />
-                            </div>
-                        </>
-                        )}
+    const excludeFields: Array<string | never[]> = [
+        "@context",
+        "@id",
+        //"@type",
+        "id",
+    ];
+
+    // Function to check if a field has the format "data-url"
+    const isDataUrlField = (field: any) => {
+        return field.format === "data-url";
+    };
+
+    if (status === "loading" || !schema || !uiSchema) {
+        return <Loading />;
+    } else if (!session) {
+        signIn('keycloak');
+    } else {
+        return (
+            <div className="container mx-auto max-w-7xl items-center justify-between p-6 lg:px-8">
+                <Head>
+                    <title>{t('things.new.title')}</title>
+                </Head>
+                <div role="presentation" className="mb-8">
+                    <Breadcrumbs aria-label="breadcrumb" data-testid="thing-breadcrumb">
+                    <Link href="/things" className="hover:underline">
+                        Things
+                    </Link>
+                    <Typography color="text.primary">{t('things.new.title')}</Typography>
+                    </Breadcrumbs>
+                </div>
+                <Container maxWidth="xl">
+                    <Grid container spacing={2}>
+                        <Grid item xs={6} md={8}>
+                            <Button onClick={handleButtonClick}>
+                                {isGridVisible ? t('things.edit.hideExpertMode') : t('things.edit.showExpertMode')}
+                            </Button>
+                        </Grid>
+                        <Grid item xs={4} md={2}>
+                            <span>Select Form</span>
+                                <select value={selectedForm} onChange={handleFormSelect}>
+                                    {formList.map((form: { id: string, name: string }) => (
+                                        <option key={form.id} value={form.id}>{form.name}</option>
+                                    ))}
+                                </select>
+                        </Grid>
                     </Grid>
-                    {isGridVisible && (
-                        <Grid item xs={6} md={4}>
-                            <h2>Select Form</h2>
-                            <select value={selectedForm} onChange={handleFormSelect}>
-                                <option value="">Select a form</option>
-                                {formList.map((form: { id: string, name: string }) => (
-                                    <option key={form.id} value={form.id}>{form.name}</option>
-                                ))}
-                            </select>
+                    <Grid container spacing={2}>
+                        <Grid item xs={isGridVisible ? 6 : 12} md={isGridVisible ? 8 : 12}>
                             {selectedForm && (
                                 <>
-                                    <Editors
-                                        formData={formData}
-                                        setFormData={setFormData}
+                                    <div>
+                                    <CustomForm
                                         schema={schema}
-                                        setSchema={setSchema}
                                         uiSchema={uiSchema}
-                                        setUiSchema={setUiSchema}
-                                        extraErrors={extraErrors}
-                                        setExtraErrors={setExtraErrors}
-                                        setShareURL={setShareURL}
-                                    />
+                                        formData={formData}
+                                        validator={validator}
+                                        onSubmit={handleSubmit}
+                                        onChange={onFormDataChange}
+                                        />
+                                    </div>
                                 </>
                             )}
                         </Grid>
-                    )}
-                </Grid>
-            </Container>
-        </>
-    );
+                        {isGridVisible && (
+                            <Grid item xs={6} md={4}>
+                                {selectedForm && (
+                                    <>
+                                        <Editors
+                                            formData={formData}
+                                            setFormData={setFormData}
+                                            schema={schema}
+                                            setSchema={setSchema}
+                                            uiSchema={uiSchema}
+                                            setUiSchema={setUiSchema}
+                                            extraErrors={extraErrors}
+                                            setExtraErrors={setExtraErrors}
+                                            setShareURL={setShareURL}
+                                        />
+                                    </>
+                                )}
+                            </Grid>
+                        )}
+                    </Grid>
+                </Container>
+            </div>
+        );
+    }
 };
-
-export default NewThingForm;
--- pwa/pages/things/new/index.tsx
+++ pwa/pages/things/new/index.tsx
@@ -1,201 +1,42 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { ErrorSchema, RJSFSchema, RJSFValidationError, UiSchema, ValidatorType } from '@rjsf/utils';
-import validator from '@rjsf/validator-ajv8';
-import { FormProps, IChangeEvent, withTheme } from '@rjsf/core';
-import Form from '@rjsf/mui';
-import Head from "next/head";
-import Editors from '@/components/form/Editors';
-import { Container, Grid, Button } from '@mui/material';
-import { signIn, useSession } from "next-auth/react";
-import { useTranslation } from 'next-i18next';
-import CustomForm from '@/components/form/CustomForm';
-
-interface Session {
-    accessToken: string;
-    error: string;
-}
-
-const METHOD = 'POST';
-const CONTENT_TYPE_HEADER = 'Content-Type';
-const AUTHORIZATION_HEADER = 'Authorization';
-const BEARER_PREFIX = 'Bearer';
-const APPLICATION_JSON = 'application/json';
-
-const NewThingForm = () => {
-    const [schema, setSchema] = useState({});
-    const [uiSchema, setUiSchema] = useState({});
-    const [formData, setFormData] = useState({});
-    const [selectedForm, setSelectedForm] = useState('');
-    const [formList, setFormList] = useState([]);
-    const [extraErrors, setExtraErrors] = useState<ErrorSchema | undefined>();
-    const [shareURL, setShareURL] = useState<string | null>(null);
-    const [isGridVisible, setGridVisible] = useState(false);
-    const { data: session = { accessToken: '', error: '' }, status } = useSession() || {};
-    const { t } = useTranslation('common');
-
-    const handleButtonClick = () => {
-        setGridVisible(!isGridVisible);
-    };
-    
-    // Fetch the list of available forms from the API
-    useEffect(() => {
-        const fetchFormList = async () => {
-            try {
-                const response = await fetch('/forms.json');
-                const data = await response.json();
-                console.log("set form list");
-                setFormList(data);
-
-                const defaultForm = data.filter((element: { code: string }) =>
-                    element.code === "place"
-                );
-
-                console.log("defaultForm");
-                console.log(defaultForm);
-                setSelectedForm(defaultForm[0].id)
-                setSchema(defaultForm[0].JSONSchema);
-                setUiSchema(defaultForm[0].UISchema);
-                setFormData(defaultForm[0].formData);
-
-            } catch (error) {
-                console.error('Error fetching form list:', error);
-            }
-        };
-
-        fetchFormList();
-    }, []);
-
-    useEffect(() => {
-        // Fetch the JSONSchema and UISchema from the API based on the selected form
-        const fetchForm = async () => {
-            try {
-                const response = await fetch(`/forms/${selectedForm}.json`);
-                const data = await response.json();
-                setSchema(data.JSONSchema);
-                setUiSchema(data.UISchema);
-                setFormData(data.formData);
-            } catch (error) {
-                console.error('Error fetching form:', error);
-            }
-        };
-
-        if (selectedForm) {
-            fetchForm();
-        } else {
-            console.log("no form selected");
+import type {
+    GetServerSideProps,
+    InferGetServerSidePropsType,
+  } from 'next';
+  
+  import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+  
+  import { New } from "@/components/thing/New";
+  import { Thing } from "@/types/Thing";
+  import { type FetchResponse, fetch } from "@/utils/dataAccess";
+  
+  export const getServerSideProps: GetServerSideProps<{
+    data: Thing,
+    hubURL: string | null,
+    page: number, // required for reviews pagination, prevents useRouter
+  }> = async ({ query: { id, page }, locale }) => {
+    try {
+      const response: FetchResponse<Thing> | undefined = await fetch(`/things/${id}`, {
+        headers: {
         }
-    }, [selectedForm]);
-
-    const handleFormSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        setSelectedForm(e.target.value);
-    };
-
-    const handleFormDataChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        try {
-            const newFormData = JSON.parse(e.target.value);
-            setFormData(newFormData);
-        } catch (error) {
-            console.error('Invalid JSON');
-        }
-    };
-
-    const handleSubmit = async (data: any) => {
-        // Handle form submission here
-        console.log(data);
-        console.log(data["formData"]);
-
-        try {
-            //if (!session) return;
-            //@ts-ignore
-            const token = session.accessToken; // Get the authentication token from the session
-            const response = await fetch('/things', {
-                method: METHOD,
-                headers: {
-                    [CONTENT_TYPE_HEADER]: APPLICATION_JSON,
-                    [AUTHORIZATION_HEADER]: `${BEARER_PREFIX} ${token}` // Include the authentication token in the request headers
-                },
-                body: JSON.stringify(data["formData"]) // Set the request body as data["formData"]
-            });
-
-            if (response.ok) {
-                // Handle successful response
-                console.log('Thing created successfully');
-            } else {
-                // Handle error response
-                console.error('Error creating thing:', response.statusText);
-            }
-        } catch (error) {
-            console.error('Error creating thing:', error);
-        }
-    };
-
-    const onFormDataChange = useCallback(
-        ({ formData }: IChangeEvent, id?: string) => {
-            if (id) {
-                console.log('Field changed, id: ', id);
-            }
-
-            setFormData(formData);
-            setShareURL(null);
-        },
-        [setFormData, setShareURL]
-    );
-
-    return (
-        <>
-            <Head>
-                <title>New Thing</title>
-            </Head>
-            <Container maxWidth="xl">
-            <Button onClick={handleButtonClick}>
-                {isGridVisible ? 'Hide Expert Mode' : 'Show Expert Mode'}
-            </Button>
-                <Grid container spacing={2}>
-                    <Grid item xs={isGridVisible ? 6 : 12} md={isGridVisible ? 8 : 12}>
-                        {selectedForm && (
-                        <>
-                            <div>
-                                <CustomForm
-                                    schema={schema}
-                                    uiSchema={uiSchema}
-                                    formData={formData}
-                                    onSubmit={handleSubmit}
-                                    validator={validator}
-                                    onChange={onFormDataChange}
-                                />
-                            </div>
-                        </>
-                        )}
-                    </Grid>
-                    {isGridVisible && (
-                        <Grid item xs={6} md={4}>
-                            <h2>Select Form</h2>
-                            <select value={selectedForm} onChange={handleFormSelect}>
-                                {formList.map((form: { id: string, name: string }) => (
-                                    <option key={form.id} value={form.id}>{form.name}</option>
-                                ))}
-                            </select>
-                            {selectedForm && (
-                                <>
-                                    <Editors
-                                        formData={formData}
-                                        setFormData={setFormData}
-                                        schema={schema}
-                                        setSchema={setSchema}
-                                        uiSchema={uiSchema}
-                                        setUiSchema={setUiSchema}
-                                        extraErrors={extraErrors}
-                                        setExtraErrors={setExtraErrors}
-                                        setShareURL={setShareURL}
-                                    />
-                                </>
-                            )}
-                        </Grid>
-                    )}
-                </Grid>
-            </Container>
-        </>
-    );
-};
-
-export default NewThingForm;
+      });
+      if (!response?.data) {
+        throw new Error(`Unable to retrieve data from /things/${id}.`);
+      }
+  
+      return { props: { 
+        data: response.data, 
+        hubURL: response.hubURL, 
+        page: Number(page ?? 1),
+        ...(await serverSideTranslations(locale ?? 'en', [
+          'common'
+          ])),
+        } 
+      };
+    } catch (error) {
+      console.error(error);
+    }
+  
+    return { notFound: true };
+  };
+  
+  export default New;
